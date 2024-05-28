8e47c22fe01734559827244b0e49737afe2ef381
Marcos Felipe <marcos.msouza@mercadolivre.com>
2024-05-28 18:14:38 -0300

    initial commit

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,40 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by mfelipesp on 28/05/24.
+//
+
+import SwiftUI
+
+/// use the main color for a view’s background
+/// accent color for a view’s text.
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by mfelipesp on 28/05/24.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
