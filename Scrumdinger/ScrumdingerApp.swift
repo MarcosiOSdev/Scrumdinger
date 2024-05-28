8f2368ed6a33ad814973de10e3298e65280b8bc0
Marcos Felipe <marcos.msouza@mercadolivre.com>
2024-05-28 13:35:28 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by mfelipesp on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
