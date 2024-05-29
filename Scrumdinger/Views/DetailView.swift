ed529b1ffa664992e8e8813403b647186402cc88
Marcos Felipe <marcos.msouza@mercadolivre.com>
2024-05-28 23:44:49 -0300

    Initial Commit

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by mfelipesp on 28/05/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,37 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by mfelipesp on 28/05/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink {
+                    Text(scrum.title)
+                } label: {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrum")
+            .toolbar {
+                Button(action: {}, label: {
+                    Image(systemName: "plus")
+                })
+                .accessibilityLabel("New scrum")
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
